4eb7dff344c101c2350614eadc420cd6cbfc7433
Valery Trakala <valery_trakala@epam.com>
2023-11-01 17:54:34 +0100

    Initial Commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Valery Trakala on 01/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
