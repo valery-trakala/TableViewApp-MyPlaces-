be0eb2220857f39c176fa17568b662fd0d98411b
Valery Trakala <valery_trakala@epam.com>
2023-11-05 11:23:41 +0100

    Initial

---
+++ TableViewApp/MainVC.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Valery Trakala on 01/11/2023.
+//
+
+import UIKit
+
+class MainVC: UITableViewController {
+    let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "XO", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "LoveLife", "Шок", "Бочка"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let restaurantName = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantName)
+        cell.textLabel?.text = restaurantName
+        
+        return cell
+    }
+}
+
